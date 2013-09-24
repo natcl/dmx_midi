cb022a5dfef860eb990cacf3f8a4d15e38c89fa8
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-09-24 19:50:15 -0400

    optimized code

--- dmx_midi_teensy/dmx_midi_teensy.ino
+++ dmx_midi_teensy/dmx_midi_teensy.ino
@@ -12,13 +12,13 @@ void loop() {
   {
     if (usbMIDI.getType() == 7)
     {
-      int channel_a = usbMIDI.getSysExArray()[3];
-      int channel_b = usbMIDI.getSysExArray()[4];
-      int channel = channel_a & 127 | channel_b << 7;
+      char channel_a = usbMIDI.getSysExArray()[3];
+      char channel_b = usbMIDI.getSysExArray()[4];
+      int channel = seven_to_fourteen(channel_a, channel_b);
       
       char value_a = usbMIDI.getSysExArray()[1];
       char value_b = usbMIDI.getSysExArray()[2];
-      char value = value_a & 127 | value_b << 7;
+      char value = seven_to_fourteen(value_a, value_b);
       
       DmxSimple.write(channel, value);
       
@@ -31,7 +31,10 @@ void loop() {
   }
 }
 
-
+// Converts two 7-bit bytes into a 14-bit int
+int seven_to_fourteen(char byte_a, char byte_b) {
+  return byte_a & 127 | byte_b << 7;
+}
 
 
 
